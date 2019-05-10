668568e63fd0ce78f6a6ba91c6c29a0478c8fed8
Xing Shi Cai <newptcai@gmail.com>
2019-05-10 19:10:46 +0200

    Files generated by PkgTemplates

---
+++ src/RandomTree.jl
@@ -0,0 +1,5 @@
+module RandomTree
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using RandomTree
+using Test
+
+@testset "RandomTree.jl" begin
+    # Write your own tests here.
+end
